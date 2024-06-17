59fe6428f1d8760f7905998ce6184568ccdb10f5
TunaKadirBerkay <Kadir.Tuna@Student.HTW-Berlin.de>
2024-06-17 17:41:52 +0200

    TestDBConnection inaktiv

--- src/main/java/TestDBConnection.java
+++ src/main/java/TestDBConnection.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+/** package com.example.demo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,3 +22,4 @@ public class TestDBConnection {
         }
     }
 }
+**/
