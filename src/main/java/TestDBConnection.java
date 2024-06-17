63e71402f1c7c22ff3acdb32c86fee4fe59811d2
TunaKadirBerkay <Kadir.Tuna@Student.HTW-Berlin.de>
2024-06-17 17:30:30 +0200

    TestDBConnection

---
+++ src/main/java/TestDBConnection.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestDBConnection {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:postgresql://dpg-cpn17duehbks7380eta0-a.oregon-postgres.render.com:5432/berkay_db";
+        String username = "berkay_db_user";
+        String password = "mrS7KP3auNE70LJcC042UMtX33mJKrTY";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to connect to the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
